3c91dbc5e2309395f5d5ae3f88b8330a81db725f
Muhammad Ahmed <ehmad1@gmail.com>
2022-02-17 20:54:45 +0500

    code formatting

--- netsuite-rest.test.js
+++ netsuite-rest.test.js
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv').config();
-var NsApiWrapper = require('./netsuite-rest.js');
+const dotenv = require("dotenv").config();
+var NsApiWrapper = require("./netsuite-rest.js");
 
-describe('Netsuite Rest Webservices', () => {
+describe("Netsuite Rest Webservices", () => {
   jest.setTimeout(10000);
   let NsApi;
   let NsApiBaseUrl;
@@ -12,7 +12,7 @@ describe('Netsuite Rest Webservices', () => {
       consumer_secret_key: process.env.consumer_secret_key,
       token: process.env.token,
       token_secret: process.env.token_secret,
-      realm: process.env.realm
+      realm: process.env.realm,
     });
 
     // with base_url
@@ -22,14 +22,13 @@ describe('Netsuite Rest Webservices', () => {
       token: process.env.token,
       token_secret: process.env.token_secret,
       realm: process.env.realm,
-      base_url: process.env.base_url
+      base_url: process.env.base_url,
     });
-
   });
 
-  afterAll(async () => { });
+  afterAll(async () => {});
 
-  it('should check env and NsApi', () => {
+  it("should check env and NsApi", () => {
     expect(process.env.consumer_key).toBeDefined();
     expect(process.env.consumer_secret_key).toBeDefined();
     expect(process.env.token).toBeDefined();
@@ -38,83 +37,66 @@ describe('Netsuite Rest Webservices', () => {
     expect(NsApi).toBeDefined();
   });
 
-  test('should make test request', () => {
+  test("should make test request", () => {
     expect.assertions(1);
     return NsApi.request({
-      method: "OPTIONS"
+      method: "OPTIONS",
     })
-      .then(response => expect(response.statusCode).toEqual(204))
+      .then((response) => expect(response.statusCode).toEqual(204))
       .catch(() => {
-        console.log("Test request failed.")
+        console.log("Test request failed.");
       });
   });
 
-  test('should make GET request - GET Customers', () => {
+  test("should make GET request - GET Customers", () => {
     expect.assertions(1);
     return NsApi.request({
-      path: 'record/v1/customer/'
+      path: "record/v1/customer/",
     })
-      .then(response => expect(response.statusCode).toEqual(200))
+      .then((response) => expect(response.statusCode).toEqual(200))
       .catch(() => {
-        console.log("GET request failed.")
+        console.log("GET request failed.");
       });
   });
 
-  test('should make POST request - SuiteQL Query', () => {
+  test("should make POST request - SuiteQL Query", () => {
     expect.assertions(1);
     return NsApi.request({
-      path: 'query/v1/suiteql?limit=5',
+      path: "query/v1/suiteql?limit=5",
       method: "POST",
       body: `{
                    "q": "SELECT id, companyName, email, dateCreated FROM customer WHERE dateCreated >= '01/01/2019' AND dateCreated < '01/01/2020'"
-                }`
+                }`,
     })
-      .then(response => expect(response.statusCode).toEqual(200))
+      .then((response) => expect(response.statusCode).toEqual(200))
       .catch(() => {
-        console.log("POST request failed.")
+        console.log("POST request failed.");
       });
   });
 
-  test('should work with base_url', () => {
+  test("should work with base_url", () => {
     expect.assertions(2);
     expect(process.env.base_url).toBeDefined();
     return NsApiBaseUrl.request({
-      method: "OPTIONS"
+      method: "OPTIONS",
     })
-      .then(response => expect(response.statusCode).toEqual(204))
+      .then((response) => expect(response.statusCode).toEqual(204))
       .catch(() => {
-        console.log("Test request failed.")
+        console.log("Test request failed.");
       });
   });
 
-  test('should work with additional headers', () => {
+  test("should work with additional headers", () => {
     expect.assertions(1);
     return NsApi.request({
       method: "OPTIONS",
       heads: {
-        foo: 'foo'
-      }
+        foo: "foo",
+      },
     })
-      .then(response => expect(response.statusCode).toEqual(204))
+      .then((response) => expect(response.statusCode).toEqual(204))
       .catch(() => {
-        console.log("Test request failed.")
+        console.log("Test request failed.");
       });
-  });
-
-  test('sandbox realms should work', async () => {
-    expect.assertions(1);
-    let sandbox = new NsApiWrapper({
-      consumer_key: process.env.consumer_key,
-      consumer_secret_key: process.env.consumer_secret_key,
-      token: process.env.token,
-      token_secret: process.env.token_secret,
-      realm: "123456XX_SB3"
-    });
-    await expect(
-      sandbox.request({
-        method: "OPTIONS",
-      })
-    ).rejects.toThrow(Error);
-
   });
 });
